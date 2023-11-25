c4c2d30a855de24b9764c5b17bc3b53914b49eac
Sandro <34787365+sjanita-obvez@users.noreply.github.com>
2023-11-24 21:06:13 -0800

    Update run-obveasy.sh

--- run-obveasy.sh
+++ run-obveasy.sh
@@ -38,6 +38,11 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 
+## create variables for docker-compose file
+export pg_pw=$1
+export af_image=$2
+export af_pw=$3
+
 
 ## create the docker compose yaml file
 echo "Creating the docker compose file"
@@ -48,6 +53,72 @@ services:
   # ===================
   # Observeasy Services
   # ===================
+  observeasy-database:
+    hostname: observeasy-database
+    container_name: observeasy-database
+    image: 'postgres:16-alpine'
+    restart: unless-stopped
+    volumes:
+      - ./database/observeasy.sql:/docker-entrypoint-initdb.d/init.sql
+      - ./database/database-data:/var/lib/postgresql/data
+    environment:
+      POSTGRES_DB: observeasy
+      POSTGRES_USER: postgres
+      POSTGRES_PASSWORD:"$pg_pw"
+    networks:
+      - observeasy
+
+  observeasy-autoflow:
+    hostname: observeasy-autoflow
+    container_name: observeasy-autoflow
+    image: '$af_image'
+    restart: unless-stopped
+    stdin_open: true
+    tty: true
+    ports:
+      - 4000:4000
+      - 9090:9090
+      - 9091:9091
+    volumes:
+      - ./autoflow/autoflow-data:/app/data
+      - ./autoflow/config.json:/app/data/config.json
+    environment:
+      AUTOFLOW_UI_PORT: 4000
+      AUTOFLOW_BOOT_FILE: data/config.json
+      AUTOFLOW_ADMIN_USERNAME: admin
+      AUTOFLOW_ADMIN_PASSWORD: "$af_pw"
+      AUTOFLOW_LICENSE_FILE: # TODO: Need to update with jrc autoflow license
+      DATABASE_HOST: observeasy-database
+      DATABASE_NAME: observeasy
+      DATABASE_USER: postgres
+      DATABASE_PASSWORD: "$pg_pw"
+      PHPIPAM_APP_ID: observeasy
+      PHPIPAM_APP_CODE: f_T_icyLF6WR7D3jseVng9ADMbrtPGlR
+      PHPIPAM_BASE_URI: phpipam-web
+      PHPIPAM_USERNAME: admin
+      PHPIPAM_PASSWORD: ipamadmin
+    depends_on:
+      - observeasy-database
+    networks:
+      - observeasy
+
+  observeasy-frontend:
+    hostname: observeasy-frontend
+    container_name: observeasy-frontend
+    image: 'dnwo05/observeasy-dockerized:latest' # TODO: Need to update with jrc docker account
+    restart: unless-stopped
+    ports:
+      - '8080:80'
+    depends_on:
+      - observeasy-database
+      - observeasy-autoflow
+
+
+networks:
+  observeasy:
+
+volumes:
+  autoflow:
 
 EOF
 
@@ -56,6 +127,3 @@ EOF
 echo "Deploying Observeasy"
 hash -r
 sudo docker-compose up --detach --force-recreate --remove-orphans
-
-
-
